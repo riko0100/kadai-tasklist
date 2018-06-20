bf60c5f63e32ae78c77f75592c3c3218ea725b67
riko.matsunaga <riko.matsunaga@rakuten.com>
2018-06-20 01:47:36 +0000

    user registration

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ Route::get('/', function () {
 });
 
 Route::resource('tasks', 'TasksController'); 
+
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
+
